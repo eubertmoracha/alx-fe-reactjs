16e0d9da18fd6b06e377cff878da0233bb85bdb5
Eubert Moracha <eubertmoracha@gmail.com>
2025-09-21 14:18:19 +0300

    Add components folder with SearchBar.jsx placeholder

---
+++ github-user-search/src/components/SearchBar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const SearchBar = () => {
+  return (
+    <input
+      type="text"
+      placeholder="Search GitHub users..."
+      className="border p-2 rounded w-full"
+    />
+  );
+};
+
+export default SearchBar;
